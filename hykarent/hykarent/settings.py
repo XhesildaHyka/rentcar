b83a2cfce04f1facd16712c31b50f2b6e007244e
XhesildaHyka <hykaxhesi1@gmail.com>
2025-07-01 18:50:26 +0200

    Update settings.py

--- hykarent/hykarent/settings.py
+++ hykarent/hykarent/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hykarent.aplication',
+    'aplication',
     'phonenumber_field',
 
 ]
