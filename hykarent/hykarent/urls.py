3e0d561d09fe276a2565023599487e84a529612d
XhesildaHyka <hykaxhesi1@gmail.com>
2025-07-01 18:51:03 +0200

    Update urls.py

--- hykarent/hykarent/urls.py
+++ hykarent/hykarent/urls.py
@@ -21,9 +21,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('hykarent.aplication.urls')),
-
+    path('', include('aplication.urls')),  # Just 'aplication.urls'
 ]
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
