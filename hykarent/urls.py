2f0450d304a514cc7eec0d88c069f22e6f2d8be5
XhesildaHyka <hykaxhesi1@gmail.com>
2025-07-01 19:04:04 +0200

    Update urls.py

--- hykarent/urls.py
+++ hykarent/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('aplication.urls')),  # your app urls
+    path('', include('hykarent.aplication.urls')),   # your app urls
 ]
 
 if settings.DEBUG:
