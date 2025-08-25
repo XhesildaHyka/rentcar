190a695ef731767a6165e1afc6309e676bb3a917
XhesildaHyka <hykaxhesi1@gmail.com>
2025-08-25 22:12:55 +0200

    change3

--- hykarent/aplication/urls.py
+++ hykarent/aplication/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 from django.contrib.sitemaps.views import sitemap
 from .sitemaps import StaticViewSitemap, CarSitemap
+from django.views.generic import TemplateView
 
 sitemaps = {
     'static': StaticViewSitemap,
@@ -24,6 +25,7 @@ urlpatterns = [
     path("luxury/", views.luxury, name="luxury"),
     path("about/", views.about, name="about"),
     path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="django.contrib.sitemaps.views.sitemap"),
+    path('googleaf7b57025ab1bb4f.html',TemplateView.as_view(template_name='googleaf7b57025ab1bb4f.html', content_type='text/html')),
     
 ]
 
